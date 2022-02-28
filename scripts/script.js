0ec1d7ad0f8a2bd2df3ab358059ba092eb70414c
Pozdneeva Nataliya <pozdneevanatalia@gmail.com>
2022-02-28 14:28:22 +0300

    Правки после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 let openPopup = document.querySelector('.profile__edit-button');
 let closePopup = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
-let likes = document.querySelectorAll('.content__card-like');
 let formElement = document.querySelector('.popup__container');
-let nameInput =  document.querySelector('.popup__name');
-let activityInput = document.querySelector('.popup__activity');
+let nameInput =  document.querySelector('.popup__input_input_name');
+let activityInput = document.querySelector('.popup__input_input_activity');
 let profileName = document.querySelector('.profile__name');
 let profileActivity = document.querySelector('.profile__activity');
 
 
 function showPopup() {
+    nameInput.value = profileName.textContent;
+    activityInput.value = profileActivity.textContent;
+
     popup.classList.add('popup_opened');
 }
-openPopup.addEventListener('click', showPopup);
 
 
 function hiddenPopup() {
     popup.classList.remove('popup_opened');
 }
-closePopup.addEventListener('click', hiddenPopup)
 
 
 function formSubmitHandler (evt) {
@@ -26,13 +26,18 @@ function formSubmitHandler (evt) {
 
     profileName.textContent = nameInput.value;
     profileActivity.textContent = activityInput.value;
-    popup.classList.remove('popup_opened');
+    hiddenPopup();
 }
+
+
+openPopup.addEventListener('click', showPopup);
+closePopup.addEventListener('click', hiddenPopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
-likes.forEach((like) => {
-    like.addEventListener('click', () => {
-        like.classList.toggle('content__card-like_active');
-    });
-});
+// let likes = document.querySelectorAll('.content__card-like');
+// likes.forEach((like) => {
+//     like.addEventListener('click', () => {
+//         like.classList.toggle('content__card-like_active');
+//     });
+// });
